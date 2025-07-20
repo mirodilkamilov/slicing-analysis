8319081cc4b4ca7a232849d5964fb93bb6b54f99
mirodilkamilov <mirodilkamilov1999@gmail.com>
2025-07-20 17:55:22 +0200

    Only add viable nodes to PDG

--- src/de/uni_passau/fim/se2/sa/slicing/graph/ProgramDependenceGraph.java
+++ src/de/uni_passau/fim/se2/sa/slicing/graph/ProgramDependenceGraph.java
@@ -2,10 +2,9 @@ package de.uni_passau.fim.se2.sa.slicing.graph;
 
 import de.uni_passau.fim.se2.sa.slicing.cfg.Node;
 import de.uni_passau.fim.se2.sa.slicing.cfg.ProgramGraph;
+import java.util.Set;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
-
-import java.util.Set;
 
 /** Provides an analysis that calculates the program-dependence graph. */
 public class ProgramDependenceGraph extends Graph implements Sliceable<Node> {
@@ -55,10 +54,13 @@ public class ProgramDependenceGraph extends Graph implements Sliceable<Node> {
 
     // Add nodes and edges from CDG
     for (Node source : cdg.getNodes()) {
-      pdg.addNode(source);
-      for (Node target : cfg.getSuccessors(source)) {
-        pdg.addNode(target);
-        pdg.addEdge(source, target);
+      var successors = cdg.getSuccessors(source);
+      if (!successors.isEmpty()) {
+        pdg.addNode(source);
+        for (Node target : successors) {
+          pdg.addNode(target);
+          pdg.addEdge(source, target);
+        }
       }
     }
 
